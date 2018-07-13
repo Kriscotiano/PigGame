2246c35e57eb0fd9e62018d0fa0aef74edd674c1
Kriscotiano <christian.o-10@hotmail.com>
2018-07-12 22:44:43 -0500

    Added active player toggling, current & total score calculating, conditional reset upon rolling a 1 & winner's display

--- starter/app.js
+++ starter/app.js
@@ -35,15 +35,41 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     diceDOM.src = 'dice-' + dice + '.png';
 
     if (dice !== 1) {
-        //Add score
+        //Add current score
         roundScore += dice;
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
         //Next player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        nextPlayer();
+    }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    //Add current score to global score
+    scores[activePlayer] += roundScore;
+
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    if (scores[activePlayer] >= 100) {
+        document.querySelector(('#name-') + activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector(('.player-') + activePlayer + '-panel').classList.add('winner');
+        document.querySelector(('.player-') + activePlayer + '-panel').classList.remove('active');
+    } else {
+    nextPlayer();
+
+    }
+});
+
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
         roundScore = 0;
 
         document.getElementById('current-0').textContent = '0';
         document.getElementById('current-1').textContent = '0';
-    }
-});
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none';
+}
